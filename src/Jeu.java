f261bcbd4e604ab5a66d2fc044f5ceb4450082e9
bernardthibaut <thibaut.t.bernard@gmail.com>
2015-10-09 19:43:34 +0200

    Ajout yeux + clic sur oiseau + bec fonctionnel (sauf déplacement)

--- src/Jeu.java
+++ src/Jeu.java
@@ -1,6 +1,10 @@
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.util.ArrayList;
+import java.util.Random;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -12,29 +16,33 @@ import sun.security.krb5.internal.Ticket;
 
 public class Jeu extends JPanel {
 
-	Oiseau o = new Oiseau(20);
+	Random r = new Random();
+
+	Oiseau o = new Oiseau(r.nextInt(10) + 40);
 	ArrayList<Ennemi> ennemis = new ArrayList<Ennemi>();
 
 	public Jeu() {
 		for (int i = 0; i < 5; i++) {
-			ennemis.add(new Ennemi(20));
+			ennemis.add(new Ennemi(r.nextInt(10) + 40));
 		}
-		
-		TimerTask task = new TimerTask() {
+
+		this.addMouseListener(new MouseAdapter() {
 			
 			@Override
-			public void run() {
+			public void mouseReleased(MouseEvent arg0) {
 				// TODO Auto-generated method stub
-				o.bouger(1, 0);
-				repaint(o.co.x-5, o.co.y-5, o.taille*2+5, o.taille*2+5);
+				lancerOiseau();
 			}
-			 
-		};
+			
+			@Override
+			public void mousePressed(MouseEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
 		
+		// lancerOiseau();
 
-		Timer timer = new Timer();
-		timer.scheduleAtFixedRate(task, 0, 10);
-		
 	}
 
 	public void paintComponent(Graphics g) {
@@ -44,16 +52,25 @@ public class Jeu extends JPanel {
 		for (Ennemi e : ennemis) {
 			paintEntite(e, g);
 		}
-		
+
 	}
 
 	public void paintEntite(Entite e, Graphics g) {
 		g.setColor(e.couleurPrincipale);
-		g.fillOval(e.co.x, e.co.y, 2 * e.taille, 2 * e.taille);
+		g.fillOval(e.co.x, e.co.y, e.taille, e.taille);
 		g.setColor(e.couleurSecondaire);
-		g.fillOval(e.co.x, e.co.y + e.taille, 2 * e.taille, e.taille);
+		g.fillOval(e.co.x + 3, e.co.y + e.taille / 2, e.taille - 6, e.taille / 2);
+		g.setColor(Color.WHITE);
+		g.fillOval(e.co.x + e.taille / 4, e.co.y + e.taille / 4, e.taille / 5, e.taille / 5);
+		g.fillOval(e.co.x + e.taille / 2 + e.taille / 10, e.co.y + e.taille / 4, e.taille / 5, e.taille / 5);
+		g.setColor(Color.BLACK);
+		g.fillOval(e.co.x + e.taille / 4 + e.taille / 10, e.co.y + e.taille / 4 + e.taille / 15, e.taille / 10,
+				e.taille / 10);
+		g.fillOval(e.co.x + e.taille / 2 + e.taille / 10, e.co.y + e.taille / 4 + e.taille / 15, e.taille / 10,
+				e.taille / 10);
+
 		if (e instanceof Oiseau) {
-			g.setColor(Color.BLACK);
+			g.setColor(Color.WHITE);
 			g.fillPolygon(o.px, o.py, 3);
 		}
 	}
@@ -64,5 +81,23 @@ public class Jeu extends JPanel {
 
 	public boolean collision(Oiseau o, Ennemi e) {
 		return (Math.sqrt(square(e.co.y - o.co.y) + square(e.co.x - o.co.x)) <= (o.taille + e.taille));
+	}
+
+	public void lancerOiseau() {
+		TimerTask task = new TimerTask() {
+
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				o.bouger(1, 0);
+				// repaint(o.co.x - 5, o.co.y - 5, o.taille * 2 + 5, o.taille *
+				// 2 + 5);
+				repaint();
+			}
+
+		};
+
+		Timer timer = new Timer();
+		timer.scheduleAtFixedRate(task, 0, 10);
 	}
 }
--- src/Oiseau.java
+++ src/Oiseau.java
@@ -2,26 +2,28 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Point;
 
+public class Oiseau extends Entite {
 
-public class Oiseau extends Entite{
-	
 	int[] px = new int[3];
 	int[] py = new int[3];
-	
-	public Oiseau(int taille){
+
+	public Oiseau(int taille) {
 		this.taille = taille;
-		super.co = new Coordonne(10, 300);
+		super.co = new Coordonne(120, 400);
 		super.couleurPrincipale = Color.RED;
 		super.couleurSecondaire = Color.ORANGE.brighter();
-		
+
 		Point[] p = new Point[3];
-		p[0] = new Point(co.x+taille*2, co.y+taille/2);
-		p[1] = new Point(co.x+taille*2, co.y-taille/2);
-		p[2] = new Point(co.x+taille*3, co.y);
+		// Point du bas
+		p[0] = new Point(co.x + taille - 2, co.y + taille / 2 + taille / 4);
+		// Point du haut
+		p[1] = new Point(co.x + taille - 2, co.y + taille / 4);
+		// Point de droite
+		p[2] = new Point(co.x + taille + taille / 4, co.y + taille / 2);
 		for (int i = 0; i < 3; i++) {
 			px[i] = p[i].x;
 			py[i] = p[i].y;
 		}
 	}
-	
+
 }
