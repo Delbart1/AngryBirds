3f854972821785f047821ad9b0e696a71b3d492e
Thibaut BERNARD <thibaut.t.bernard@gmail.com>
2015-10-11 15:55:17 +0200

    Amélioration graphique

--- src/Jeu.java
+++ src/Jeu.java
@@ -2,55 +2,43 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionAdapter;
-import java.awt.event.MouseMotionListener;
 import java.util.ArrayList;
 import java.util.Random;
 import java.util.Timer;
 import java.util.TimerTask;
 
 import javax.swing.ImageIcon;
-import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javafx.scene.shape.Rectangle;
-import sun.security.krb5.internal.Ticket;
-
 
 /**
  * classe du jeu, avec les actions
  * 
  * @author youdelice
  */
+@SuppressWarnings("serial")
 public class Jeu extends JPanel {
 
 	Random r = new Random();
 	boolean lance = false;
-        private int idDirection;
+	private int idDirection;
 
-	Oiseau o = new Oiseau(r.nextInt(10) + 40);
+	Oiseau o = new Oiseau(50);
 	ArrayList<Ennemi> ennemis = new ArrayList<Ennemi>();
 
 	Coordonne coInit = new Coordonne(o.co.x, o.co.y);
 
-	public Jeu() {
-		for (int i = 0; i < 5; i++) {
-			ennemis.add(new Ennemi(r.nextInt(10) + 40));
+	public Jeu(int nbEnnemis) {
+		for (int i = 0; i < nbEnnemis; i++) {
+			ennemis.add(new Ennemi(50));
 		}
 
 		this.addMouseListener(new MouseAdapter() {
 
 			@Override
 			public void mouseReleased(MouseEvent arg0) {
-				// TODO Auto-generated method stub
 				if (!lance)
 					lancerOiseau();
-			}
-
-			@Override
-			public void mousePressed(MouseEvent arg0) {
-				// TODO Auto-generated method stub
-
 			}
 		});
 
@@ -58,91 +46,134 @@ public class Jeu extends JPanel {
 
 			@Override
 			public void mouseDragged(MouseEvent e) {
-				// TODO Auto-generated method stub	
+				// TODO Auto-generated method stub
 				if (e.getX() >= coInit.x - 50 && e.getX() < coInit.x + 50 && !lance) {
-					for(int i = 0 ; i < o.px.length ; i++){
+					for (int i = 0; i < o.px.length; i++) {
 						o.px[i] += e.getX() - o.co.x;
+						o.px2[i] += e.getX() - o.co.x;
 					}
 					o.co.x = e.getX();
 					repaint();
 				}
 				if (e.getY() >= coInit.y - 50 && e.getY() < coInit.y + 50 && !lance) {
-					for(int i = 0 ; i < o.px.length ; i++){
+					for (int i = 0; i < o.px.length; i++) {
 						o.py[i] += e.getY() - o.co.y;
-                                        }
+						o.py2[i] += e.getY() - o.co.y;
+					}
 					o.co.y = e.getY();
 					repaint();
 				}
 			}
 		});
-
 	}
 
-        
-        /**
-         * IHM creant le fond et les ennemis
-         * 
-         * @param g 
-         */
+	/**
+	 * IHM creant le fond et les ennemis
+	 * 
+	 * @param g
+	 */
 	public void paintComponent(Graphics g) {
-		g.drawImage(new ImageIcon(Main.class.getResource("fond.png")).getImage(), 0, 0, null);
+		g.drawImage(new ImageIcon(Main.class.getResource("fond2.png")).getImage(), 0, 0, null);
 		paintEntite(o, g);
 
 		for (Ennemi e : ennemis) {
 			paintEntite(e, g);
 		}
 
+		g.drawImage(new ImageIcon(Main.class.getResource("caisse.png")).getImage(), 0, 480, null);
 	}
 
-        /**
-         * ihm d'une entité
-         * 
-         * @param e l'entité
-         * @param g 
-         */
+	/**
+	 * ihm d'une entité
+	 * 
+	 * @param e
+	 *            l'entité
+	 * @param g
+	 */
 	public void paintEntite(Entite e, Graphics g) {
+
+		// Cell-shading
+		g.setColor(Color.BLACK);
+		g.fillOval(e.co.x - 3, e.co.y - 3, e.taille + 6, e.taille + 6);
+
 		if (e instanceof Oiseau) {
-			g.setColor(new Color(255,204,153));
-			g.fillPolygon(o.px, o.py, 3);
-			if(!lance){
-				g.setColor(new Color(51,25,0));
+			// Elastique
+			if (!lance) {
+				g.setColor(new Color(51, 25, 0));
 				g.drawLine(o.co.x + o.taille / 2, o.co.y + o.taille / 2, 142, 435);
 			}
 		}
+
+		// Corps
 		g.setColor(e.couleurPrincipale);
 		g.fillOval(e.co.x, e.co.y, e.taille, e.taille);
-		g.setColor(e.couleurSecondaire);
-		g.fillOval(e.co.x + 3, e.co.y + e.taille / 2, e.taille - 6, e.taille / 2);
-		g.setColor(Color.WHITE);
-		g.fillOval(e.co.x + e.taille / 4, e.co.y + e.taille / 4, e.taille / 5, e.taille / 5);
-		g.fillOval(e.co.x + e.taille / 2 + e.taille / 10, e.co.y + e.taille / 4, e.taille / 5, e.taille / 5);
-		g.setColor(Color.BLACK);
-		g.fillOval(e.co.x + e.taille / 4 + e.taille / 10, e.co.y + e.taille / 4 + e.taille / 15, e.taille / 10,
-				e.taille / 10);
-		g.fillOval(e.co.x + e.taille / 2 + e.taille / 10, e.co.y + e.taille / 4 + e.taille / 15, e.taille / 10,
-				e.taille / 10);
 
+		// Corps inf�rieur
+		g.setColor(e.couleurSecondaire);
+		g.fillOval(e.co.x + 5, e.co.y + e.taille / 2, e.taille - 10, e.taille / 2);
+
+		if (e instanceof Oiseau) {
+
+			// Yeux
+			g.setColor(Color.WHITE);
+			g.fillOval(e.co.x + e.taille / 2, e.co.y + e.taille / 4, e.taille / 5, e.taille / 5);
+			g.fillOval(e.co.x + e.taille / 2 + e.taille / 5, e.co.y + e.taille / 4, e.taille / 5, e.taille / 5);
+
+			// Pupilles
+			g.setColor(Color.BLACK);
+			g.fillOval(e.co.x + e.taille / 2 + e.taille / 15, e.co.y + e.taille / 4 + e.taille / 15, e.taille / 10,
+					e.taille / 10);
+			g.fillOval(e.co.x + e.taille / 2 + e.taille / 5 + e.taille / 25, e.co.y + e.taille / 4 + e.taille / 15, e.taille / 10,
+					e.taille / 10);
+			
+			// Bec
+			g.setColor(new Color(255, 204, 0));
+			g.fillPolygon(o.px, o.py, 3);
+			g.setColor(new Color(255, 172, 0));
+			g.fillPolygon(o.px2, o.py2, 3);
+			g.setColor(Color.BLACK);
+			g.drawPolygon(o.px, o.py, 3);
+			g.drawLine(o.px2[0], o.py2[0], o.px2[1], o.py2[1]);
+			g.drawLine(o.px2[1], o.py2[1], o.px2[2], o.py2[2]);
+
+		}
+
+		else {
+			// Yeux
+			g.setColor(Color.WHITE);
+			g.fillOval(e.co.x + e.taille / 4, e.co.y + e.taille / 4, e.taille / 5, e.taille / 5);
+			g.fillOval(e.co.x + e.taille / 2 + e.taille / 10, e.co.y + e.taille / 4, e.taille / 5, e.taille / 5);
+
+			// Pupilles
+			g.setColor(Color.BLACK);
+			g.fillOval(e.co.x + e.taille / 4 + e.taille / 10, e.co.y + e.taille / 4 + e.taille / 15, e.taille / 10,
+					e.taille / 10);
+			g.fillOval(e.co.x + e.taille / 2 + e.taille / 10, e.co.y + e.taille / 4 + e.taille / 15, e.taille / 10,
+					e.taille / 10);
+		}
 
 	}
 
-        /**
-         * geré la collision entre 2 entité
-         * 
-         * @param o oiseau
-         * @param e un ennemi
-         * @return si il touche ou pas
-         */
+	/**
+	 * geré la collision entre 2 entité
+	 * 
+	 * @param o
+	 *            oiseau
+	 * @param e
+	 *            un ennemi
+	 * @return si il touche ou pas
+	 */
 	public boolean collision(Oiseau o, Ennemi e) {
 		return o.co.x < e.co.x + e.taille && o.co.x + o.taille > e.co.x && o.co.y < e.co.y + e.taille
 				&& o.co.y + o.taille > e.co.y;
 	}
 
-        /**
-         * timer du jeu, retire les ennemi si toucher et le lancer de l'oiseau 
-         * 
-         */
+	/**
+	 * timer du jeu, retire les ennemi si toucher et le lancer de l'oiseau
+	 * 
+	 */
 	public void lancerOiseau() {
-                idDirection = r.nextInt(2) + 1;
+		idDirection = r.nextInt(2) + 1;
 
 		lance = true;
 
@@ -164,8 +195,7 @@ public class Jeu extends JPanel {
 				for (Ennemi e : ennemis) {
 					if (collision(o, e)) {
 						ennemisMorts.add(e);
-                                                
-                                        
+
 					}
 				}
 				for (Ennemi e : ennemisMorts) {
--- src/Main.java
+++ src/Main.java
@@ -10,9 +10,10 @@ public class Main {
 		JFrame f = new JFrame("Angry Birds");
 		f.setIconImage(new ImageIcon(Main.class.getResource("favicon.png")).getImage());
 		f.setPreferredSize(new Dimension(800, 600));
+		f.setResizable(false);
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		f.setContentPane(new Jeu());
+		f.setContentPane(new Jeu(1));
 		
 		f.pack();
 		f.setLocationRelativeTo(null);
