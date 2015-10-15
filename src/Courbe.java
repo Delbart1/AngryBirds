fc1d906a828fb4ee6b0e247aed096375e67c9174
Jonathan DELBART <jonathan.delbart@etudiant.univ-lille1.fr>
2015-10-15 11:18:10 +0200

    courbe3

--- src/Courbe.java
+++ src/Courbe.java
@@ -5,19 +5,43 @@ import java.awt.Graphics;
 public class Courbe extends Applet {
 
 	
-	private double a=0.001;   // angle
-	private double b=-0.02;  // hauteur
-	private double c=100;   // 
+	private double a; // angle
+	private double b;// hauteur
+	private double c; //  
 	
-	/*public Courbe(double a, double b, double c){
+	public Courbe(double a, double b, double c){
 		this.a = a;
 		this.b = b;
 		this.c = c;
-	}*/
+	}
 
 	double f(double x) {
 		return  a*Math.pow(x, 2)+ b*x + c;
 	}
+	
+	  public double getCoefD(int x) {
+	        double coef1;
+	        double coef2;
+	        coef1 = (f(x) - f(x + 1));
+	        coef2 = (x - (x + 1));
+	        return coef2 / coef1;
+	    }
+
+	 
+	    public double getCoefDy(int x) {
+	        return f(x) - f(x - 1);
+	    }
+
+	    
+	    public double angleNext(int x) {
+	        double tmp = getCoefD(x);
+	        if (tmp < 0) {
+	            return Math.PI / 2 - Math.atan(1 / getCoefDy(x)) + Math.PI;
+	        } else {
+	            return Math.PI / 2 - Math.atan(1 / getCoefDy(x));
+	        }
+	    }
+	        
 
 	public void paint(Graphics g) {
 		for (int x = 0; x < getSize().width; x++) {
